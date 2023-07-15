8b47e43efd84555520af02cb9b2dd429dd68c00d
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-15 07:40:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function fibonacci(num) {
 	let val1 = 0;
 	let val2 = 1;
 
-	if(num === 0) return val1;
-	if(num === 1) return val2;
+	if(num === 1) return val1;
+	if(num === 2) return val2;
 	
-	for(let i = 2; i <= num; i++) {
+	for(let i = 3; i <= num; i++) {
 		let temp = val1 + val2;
 		val1 = val2;
 		val2 = temp;
